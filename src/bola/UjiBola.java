32af032c68e00846b5bd10e4ca8a6f6a698e6c32
DeasyNabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2021-04-29 11:02:28 +0700

    Praktikum Bola

---
+++ src/bola/UjiBola.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bola;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class UjiBola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bola ball= new Bola();
+        ball.setJari(14);
+        ball.showDiameter();
+        ball.showLuas();
+        ball.showVolume();
+        ball.cetak();
+   }
+}
+    
+    
